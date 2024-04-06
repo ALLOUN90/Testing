5b7900e099d030132221005717d78ffa0846d6dd
alloun mohamed <mohamed.moubah123@gmail.com>
2024-04-06 23:26:19 +0000

    Ajoutez des fichiers projet.

---
+++ Testing/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Data;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        { 
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
---
+++ Testing/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Testing.Models;
+
+namespace Testing.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DesplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DesplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DesplayOrder = 3 }
+                );
+
+            
+        }
+    }
+}
